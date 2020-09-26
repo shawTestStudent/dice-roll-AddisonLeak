e6575489f2adc48c984477ac6e9f4f462f704bb4
shawTestStudent <71569341+shawTestStudent@users.noreply.github.com>
2020-09-26 12:07:00 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,5 +4,7 @@ input.onGesture(Gesture.Shake, function () {
 // Hi Addison,
 // I'm not seeing any progress on your project. Please let me know if you have having any difficulty, so I can make sure you don't fall behind.
 // 
+// adding a test comment.
+// 
 // --Mr. Shaw
 basic.showString("Shake to roll the dice")
